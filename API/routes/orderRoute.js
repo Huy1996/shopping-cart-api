066ceacca90496c3518664cb243ea960775d9380
Nguyen <minhhuy1411417@gmail.com>
2022-01-10 23:11:13 -0800

    Added middleware to update number of product in stock after placing order

--- API/routes/orderRoute.js
+++ API/routes/orderRoute.js
@@ -1,26 +1,27 @@
-import express from 'express';
-import { isAdmin, isAuth } from '../middleware/middleware.js';
+import express              from 'express';
+import { isAdmin, isAuth }  from '../middleware/middleware.js';
 import * as orderController from '../controllers/orderController.js'
+import { updateProduct }    from "../middleware/helper.js";
 
 const orderRoute = express.Router();
 
 // Get Request Section
-orderRoute.get('/',             isAuth,     isAdmin,    orderController.getAllOrders);
-orderRoute.get('/summary',      isAuth,     isAdmin,    orderController.getSummary);
-orderRoute.get('/mine',         isAuth,                 orderController.getPersonalOrder);
-orderRoute.get('/:id',          isAuth,                 orderController.getOrderByID);
+orderRoute.get('/',             isAuth,     isAdmin,        orderController.getAllOrders);
+orderRoute.get('/summary',      isAuth,     isAdmin,        orderController.getSummary);
+orderRoute.get('/mine',         isAuth,                     orderController.getPersonalOrder);
+orderRoute.get('/:id',          isAuth,                     orderController.getOrderByID);
 
 
 // Post Request Section
-orderRoute.post('/',            isAuth,                 orderController.createOrder);
+orderRoute.post('/',            isAuth,     updateProduct,  orderController.createOrder);
 
 
 // Put Request Section
-orderRoute.put('/:id/pay',      isAuth,                 orderController.payForOrder);
-orderRoute.put('/:id/deliver',  isAuth,                 orderController.orderDelivered);
+orderRoute.put('/:id/pay',      isAuth,                     orderController.payForOrder);
+orderRoute.put('/:id/deliver',  isAuth,                     orderController.orderDelivered);
 
 
 // Delete Request Section
-orderRoute.delete('/:id',       isAuth,                 orderController.deleteOrder)
+orderRoute.delete('/:id',       isAuth,                     orderController.deleteOrder)
 
 export default orderRoute;
