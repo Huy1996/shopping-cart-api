5aaa18fd5db07356cc960ccabfcaf0bb23c44f68
Nguyen <minhhuy1411417@gmail.com>
2022-01-12 22:20:57 -0800

    Added middleware to for validate the review

--- API/routes/reviewRoute.js
+++ API/routes/reviewRoute.js
@@ -1,22 +1,22 @@
 import express from 'express';
 import { isAdmin, isAuth } from '../middleware/middleware.js';
 import * as reviewController from '../controllers/reviewController.js'
-import {calculateRating} from "../middleware/helper.js";
+import {calculateRating, isReviewValid} from "../middleware/helper.js";
 
 const reviewRoute = express.Router();
 
 // Get Request Section
-reviewRoute.get('/user',           isAuth,             reviewController.getReviewFromUser);
-reviewRoute.get('/product/:id',                        reviewController.getReviewFromProduct);
-reviewRoute.get('/user/:id',       isAuth, isAdmin,    reviewController.getUserReview)
+reviewRoute.get('/user',           isAuth,                  reviewController.getReviewFromUser);
+reviewRoute.get('/product/:id',                             reviewController.getReviewFromProduct);
+reviewRoute.get('/user/:id',       isAuth, isAdmin,         reviewController.getUserReview)
 
 // Post Request Section
-reviewRoute.post('/:id',           isAuth,             reviewController.createReview,           calculateRating);
+reviewRoute.post('/:id',           isAuth, isReviewValid,   reviewController.createReview,  calculateRating);
 
 // Put Request Section
-reviewRoute.put('/id',             isAuth,             reviewController.updateReview)
+reviewRoute.put('/id',             isAuth,                   reviewController.updateReview)
 
 // Delete Request Section
-reviewRoute.delete('/:id',         isAuth,             reviewController.deleteReview);
+reviewRoute.delete('/:id',         isAuth,                   reviewController.deleteReview);
 
 export default reviewRoute;
