f0efc3f1170e2a3f7314bfe9631418d2a5280311
Nguyen Duong <minhhuy1411417@gmail.com>
2022-02-11 04:22:15 -0800

    Refactor

--- API/models/product.js
+++ API/models/product.js
@@ -1,48 +1,19 @@
-
 import mongoose from 'mongoose';
 
-import Review from "./review.js";
-
-
 const productSchema = new mongoose.Schema({
-    name:{
-        type: String,
-        required: true,
-        unique: true,
-    },
-    image: {
-        type:String,
-        required: true,
-    },
-    brand: {
-        type: String,
-        required: true,
-    },
-    category: {
-        type: String,
-        required: true,
-    },
-    description: {
-        type: String,
-        required: true,
-    },
-    price: {
-        type: Number,
-        required: true,
-    },
-    countInStock: {
-        type: Number,
-        required: true,
-    },
-    rating: {
-        type: Number,
-        required: true,
-    },
-    numReviews: {
-        type: Number,
-        required: true,
-    },
-    reviews:[Review]
+    name:           { type: String, required: true },
+    image:          { type:String, required: true },
+    brand:          { type: String, required: true },
+    category:       { type: String, required: true },
+    description:    { type: String, required: true },
+    price:          { type: Number, required: true },
+    countInStock:   { type: Number, required: true },
+    rating:         { type: Number, required: true  },
+    numReviews:     { type: Number, required: true },
+    attribute: [{
+        name:       { type: String, required: true },
+        options:    [{type: String}]
+    }],
 },{
     timestamps: true,
 });
